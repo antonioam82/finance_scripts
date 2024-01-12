55c875ac117193b8231003c602f6d6cd4ccf6e4e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-01-12 10:39:19 +0100

    Update rsi.R

--- rsi.R
+++ rsi.R
@@ -5,7 +5,7 @@ library(quantmod)
 stock_symbol <- "TSLA"  # Cambia AAPL por el símbolo de la acción que desees
 
 # Obtener los datos históricos de la acción
-getSymbols(stock_symbol, src = "yahoo")
+getSymbols(stock_symbol, src = "yahoo", from = "2023-01-01")
 
 # Calcular el RSI con un período de 14 días
 rsi_14 <- RSI(Cl(get(stock_symbol)), n = 14)
@@ -14,3 +14,11 @@ rsi_14 <- RSI(Cl(get(stock_symbol)), n = 14)
 tail(rsi_14)
 
 plot(rsi_14,type='l')
+
+# Imprimir los últimos valores del RSI
+cat("Últimos valores del RSI:\n", tail(rsi_14), "\n")
+
+titulo <- paste("Precio de ", stock_symbol)
+
+# Crear un gráfico con el precio de cierre y el RSI
+chart_Series(get(stock_symbol), name = titulo, TA = "add_RSI(n = 14)")
