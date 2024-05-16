717c9f0f373db94abbd6e37586d503c4cf07c4b8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-05-16 23:27:59 +0200

    Update capitalizacion.R

--- capitalizacion.R
+++ capitalizacion.R
@@ -2,8 +2,10 @@
 #install.packages("quantmod")
 library(quantmod)
 
+
 # Definir el símbolo de la acción y el rango de fechas
 ticker <- "NVDA"  # Símbolo de Apple Inc. (puedes cambiarlo por el de otra acción)
+#ticker2 <- "INTC"
 start_date <- "2020-01-01"
 end_date <- Sys.Date()  # Fecha actual
 
@@ -22,7 +24,31 @@ plot(index(market_cap), market_cap, type = "l", col = "blue",
 # Agregar una leyenda
 legend("topleft", legend = ticker, col = "blue", lty = 1)
 
-########################### CON 'ggplot2' #########################
+#######################################33
+
+ticker2 <- "INTC"
+
+# Obtener los datos históricos de la segunda acción
+getSymbols(ticker2, from = start_date, to = end_date)
+
+# Calcular la capitalización de mercado para la segunda acción
+market_cap2 <- Cl(get(ticker2)) * Vo(get(ticker2))
+tail(market_cap2)
+
+# Graficar la evolución de la capitalización de mercado para ambas acciones
+plot(index(market_cap), market_cap, type = "l", col = "blue",
+     main = "Evolución de la Capitalización de Mercado de NVDA e INTC",
+     xlab = "Fecha", ylab = "Capitalización de Mercado (en USD)")
+lines(index(market_cap2), market_cap2, type = "l", col = "red")
+
+# Agregar una leyenda
+legend("topleft", legend = c(ticker, ticker2), col = c("blue", "red"), lty = 1)
+
+############################################################
+
+# Instalar y cargar las librerías necesarias
+#install.packages("quantmod")
+#install.packages("ggplot2")
 library(quantmod)
 library(ggplot2)
 
@@ -49,4 +75,5 @@ ggplot(market_cap_df, aes(x = Date, y = MarketCap)) +
        x = "Fecha", y = "Capitalización de Mercado (en USD)") +
   theme_minimal() +
   theme(legend.position = "top", legend.title = element_blank(), legend.text = element_text(color = "blue"))
+
 
